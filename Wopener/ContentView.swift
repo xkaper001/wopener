b0873e173bd09dde988cb13fa8022c0ed2827c4b
xkaper001 <imluckyayan@gmail.com>
2026-06-10 13:32:51 +0530

    Initial Commit

---
+++ Wopener/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wopener
+//
+//  Created by Denji on 10/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wopener/WopenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WopenerApp.swift
+//  Wopener
+//
+//  Created by Denji on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct WopenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
